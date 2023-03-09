bf62e8f8bf6f416bc3bb976ae596b5fc2f143f61
Júlio César Ködel <jckodel@gmail.com>
2023-03-09 19:43:50 -0300

    Flutter version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jckodel/fvm/versions/3.7.6"
+export "FLUTTER_ROOT=/Users/jckodel/fvm/versions/3.7.7"
 export "FLUTTER_APPLICATION_PATH=/Users/jckodel/Desktop/eva"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
